4508370e407089315e9002cc3ccd391770b05b02
Xiaoxingrenmi <1256842472@qq.com>
2018-12-19 00:19:13 +0800

    Fix DFA in html_parser

--- crawler/html_parser.c
+++ crawler/html_parser.c
@@ -121,6 +121,18 @@ void ParseAtagUrls(const char* html,
         if (*p == '>') {
           state = 0;
 
+          const char* first_space = strchr(url_beg, ' ');
+          if (first_space && first_space < url_end)
+              url_end = first_space;
+
+          const char* first_return = strchr(url_beg, '\r');
+          if (first_return && first_return < url_end)
+              url_end = first_return;
+
+          const char* first_newline = strchr(url_beg, '\n');
+          if (first_newline && first_newline < url_end)
+              url_end = first_newline;
+
           char* url = CopyrString(url_beg, url_end);
           if (url) {
             callback(url, context);
